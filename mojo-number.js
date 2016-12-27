0e019970e15eb9a4ed003d23d7a08ac566c43c6a
@supercobra <daniel@metadot.com>
2016-12-26 19:11:44 -0600

    Create mojo-number.js

---
+++ mojo-number.js
@@ -0,0 +1,17 @@
+
+"use strict";
+var fetch = require('node-fetch')
+var url = 'http://www.mojohelpdesk.com/feeds/quick_status?key=' + dddddddddddddddddddkey;
+var count = 0;
+fetch(url)
+    .then(function (res) {
+        return res.text();
+    }).then(function (data) {
+        console.log('data from fetch: ' + data);
+        if (data > 0) {
+            console.log('Montastic alarms: ' + data);
+        } else {
+            console.log('Mojo OK');
+        }
+        console.log(data);
+    });
